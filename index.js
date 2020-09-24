90969e936c4a4309d3a8b90104c93fb00d8acca2
Ahmad Awais <me@AhmadAwais.com>
2020-09-25 01:52:21 +0500

    📦 NEW: First commit

--- index.js
+++ index.js
@@ -5,4 +5,23 @@
  *
  * @author Awais <https://twitter.com/MrAhmadAwais/>
  */
-module.exports = () => {};
+module.exports = (stringData, {sentencesPerPara = 3}) => {
+	let paras = [];
+
+	// Convert string to array of sentences.
+	let raw = stringData.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
+
+	// Create paras of sentencesPerPara sentences.
+	raw.map(sentence => paras.push(raw.splice(0, sentencesPerPara)));
+
+	// Join sentences with a space if more than 1 per para.
+	if (!sentencesPerPara === 1) {
+		paras = paras.map(para => para.join(` `));
+	}
+
+	// Create <p></p> HTML for paras.
+	paras = paras.map(para => (para = `<p>${para}</p>`));
+	paras = paras.join(' ');
+
+	return paras;
+};
