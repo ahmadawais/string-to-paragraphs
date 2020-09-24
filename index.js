4a1d2874b4221a31325fdaaf21f262ad148c79ab
Ahmad Awais <me@AhmadAwais.com>
2020-09-25 01:41:20 +0500

    📦 NEW: First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * String To Paragraphs.
+ *
+ * Convert a large raw string to paragraphs of n sentences
+ *
+ * @author Awais <https://twitter.com/MrAhmadAwais/>
+ */
+module.exports = () => {};
---
+++ test.js
@@ -0,0 +1,3 @@
+const stringToParagraphs = require('./index.js');
+
+stringToParagraphs();
