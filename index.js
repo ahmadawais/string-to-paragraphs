5b2ab592b81bbef1bec3bb59d5e1539cacb0a777
Ahmad Awais <me@AhmadAwais.com>
2020-09-25 02:24:41 +0500

    🐛 FIX: WIP

--- index.js
+++ index.js
@@ -5,23 +5,38 @@
  *
  * @author Awais <https://twitter.com/MrAhmadAwais/>
  */
-module.exports = (stringData, {sentencesPerPara = 3}) => {
+module.exports = (stringData, options) => {
+	const defaultOptions = {sentencesPerPara: 3};
+	const opts = {...defaultOptions, ...options};
+	const {sentencesPerPara} = opts;
+
+	// Hold them paras.
 	let paras = [];
 
 	// Convert string to array of sentences.
 	let raw = stringData.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
+	console.log('raw: ', raw);
 
 	// Create paras of sentencesPerPara sentences.
-	raw.map(sentence => paras.push(raw.splice(0, sentencesPerPara)));
+	let count = sentencesPerPara;
+	while (count < 1) {
+		count--;
+		raw.map(sentence => paras.push(raw.splice(0, sentencesPerPara)));
+	}
+	paras.push([raw.join(' ')]);
+	console.log('paras: ', paras);
 
 	// Join sentences with a space if more than 1 per para.
-	if (!sentencesPerPara === 1) {
+	if (sentencesPerPara !== 1) {
 		paras = paras.map(para => para.join(` `));
+		console.log('paras: ', paras);
 	}
 
 	// Create <p></p> HTML for paras.
 	paras = paras.map(para => (para = `<p>${para}</p>`));
-	paras = paras.join(' ');
+	console.log('paras: ', paras);
+	paras = paras.join('');
+	console.log('paras: ', paras);
 
 	return paras;
 };
--- test.js
+++ test.js
@@ -1,3 +1,24 @@
+const assert = require('assert').strict;
 const stringToParagraphs = require('./index.js');
 
-stringToParagraphs();
+const stringData = `Sentence #1. Sentence #2. Sentence #3. Sentence #4. Sentence #5. Sentence #6. Sentence #7.`;
+
+// const parasWithThreeSentences = stringToParagraphs(stringData);
+// console.log('parasWithThreeSentences: ', parasWithThreeSentences);
+
+// assert.strictEqual(
+// 	parasWithThreeSentences,
+// 	`<p>Sentence #1. Sentence #2. Sentence #3.</p><p>Sentence #4. Sentence #5. Sentence #6. Sentence #7.</p>`,
+// 	`Failed test #1`
+// );
+
+const parasWithOneSentences = stringToParagraphs(stringData, {
+	sentencesPerPara: 2
+});
+
+console.log('parasWithOneSentences: ', parasWithOneSentences);
+// assert.strictEqual(
+// 	parasWithOneSentences,
+// 	`<p>Sentexnce #1. Sentence #2. Sentence #3.</p> <p>Sentence #4. Sentence #5. Sentence #6.</p>`,
+// 	`Failed test #2`
+// );
